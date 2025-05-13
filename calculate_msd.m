3c9dd23c318a69f4ecefbf10627291a1e2a8e948
John Marshall Cohen <jmc696@i17n2.ocelote.hpc.arizona.edu>
2025-05-12 21:58:26 -0700

    mesh modification autograder test

--- calculate_msd.m
+++ calculate_msd.m
@@ -9,24 +9,50 @@ function msd = calculate_msd(f, lambda, epsilon)
 % Output:
 %   msd     - KxL matrix, where K = numel(lambda), L = numel(epsilon)
 
-    f = double(f);  % Ensure single precision
-
-    % Run the ROF smoothing algorithm
-    u = smooth_image_rof(f, lambda, epsilon);  % u is H x W x K x L
-
+    f = double(f);
     [H, W] = size(f);
     K = numel(lambda);
     L = numel(epsilon);
+    msd = zeros(K, L);
 
-    % Expand f to match u size
-    f_rep = repmat(f, 1, 1, K, L);
+    bytes_per_element = 8;  % for double precision
+    estimated_bytes = H * W * bytes_per_element;
 
-    % Compute squared difference
-    diff = u - f_rep;
-    diff_sq = diff .^ 2;
+    for i = 1:K
+        for j = 1:L
+            % Check GPU availability and memory safety
+            use_gpu = (gpuDeviceCount > 0) && estimated_bytes < 7.5e9;
+            if use_gpu
+                f = gpuArray(f);
+            end
 
-    % Compute mean square difference
-    msd = sum(diff_sq, [1 2]) / (H * W);
-    msd = reshape(msd, K, L);   % Ensure 2D output shape
-    msd = double(msd);          % Ensure single precision output
+            % Compute ROF-smoothed result for this (lambda, epsilon) pair
+            u = smooth_image_rof(f, lambda_vals(i), epsilon_vals(j));
+
+            % Ensure it's on CPU for comparison
+            u = gather(u);
+            diff = u - f;
+            msd(i, j) = mean(diff(:).^2);
+         end
+     end
+    % f = double(f);  % Ensure single precision
+    % 
+    % % Run the ROF smoothing algorithm
+    % u = smooth_image_rof(f, lambda, epsilon);  % u is H x W x K x L
+    % 
+    % [H, W] = size(f);
+    % K = numel(lambda);
+    % L = numel(epsilon);
+    % 
+    % % Expand f to match u size
+    % f_rep = repmat(f, 1, 1, K, L);
+    % 
+    % % Compute squared difference
+    % diff = u - f_rep;
+    % diff_sq = diff .^ 2;
+    % 
+    % % Compute mean square difference
+    % msd = sum(diff_sq, [1 2]) / (H * W);
+    % msd = reshape(msd, K, L);   % Ensure 2D output shape
+    % msd = double(msd);          % Ensure single precision output
 end
